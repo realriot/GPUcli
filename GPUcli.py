66b0d0ef59730cac625ce42e09632ff00bd2aad7
Sascha Schmidt <realriot@realriot.de>
2018-10-27 17:44:32 +0200

    Initial release

---
+++ GPUcli.py
@@ -0,0 +1,225 @@
+#!/usr/bin/python
+import oauthlib, json, argparse, time, os
+from requests_oauthlib import OAuth2Session
+
+version = "1.0"
+
+# Remove already existing client permissions on first start: https://myaccount.google.com/u/0/permissions.
+
+# Define client_id und client_secret created by the google api console.
+client_id = ''
+client_secret = ''
+
+redirect_uri = 'https://localhost'
+
+# Define the scope of this app.
+scope = ['https://www.googleapis.com/auth/userinfo.email',
+         'https://www.googleapis.com/auth/userinfo.profile',
+         'https://www.googleapis.com/auth/photoslibrary']
+
+
+# Function: load token from file.
+def load_token():
+    try:
+        f = open('token.dat', 'r')
+        token = json.loads(f.read())
+        f.close()
+    except:
+        token = ""
+
+    return token
+
+
+# Function: save token to file.
+def save_token(token):
+    try:
+        f = open('token.dat', 'w')
+        f.write(json.dumps(token))
+        f.close()
+    except:
+        print "ERROR: Failed to save token data!"
+
+
+# Function: Authorize against google user account.
+def auth_app():
+    oauth = OAuth2Session(client_id, redirect_uri=redirect_uri,
+                          scope=scope)
+    authorization_url, state = oauth.authorization_url(
+        'https://accounts.google.com/o/oauth2/auth',
+        access_type="offline", prompt="consent")
+
+    print "Please authorize access for this app at:"
+    print
+    print authorization_url
+    print
+
+    print 'Afterwards google will reqirect you to an URL.'
+    authorization_response = raw_input('Please enter the full URL: ')
+
+    try:
+        token = oauth.fetch_token(
+            'https://accounts.google.com/o/oauth2/token',
+            authorization_response=authorization_response,
+            client_secret=client_secret)
+    except:
+        print "ERROR: Failed to fetch access token!"
+        exit(-1)
+
+    # Save fetched token to data file for further use.
+    save_token(token)
+
+    return token
+
+# Function: Refresh access_token
+def refresh_token():
+    extra = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+    }
+    oauth.refresh_token('https://accounts.google.com/o/oauth2/token', **extra)
+    save_token(oauth.token)
+
+# Function: Fetch user informations.
+def getUserInfo():
+    r = oauth.get('https://www.googleapis.com/oauth2/v1/userinfo')
+    return json.loads(r.text)
+
+
+# Function: Create album
+def g_createAlbum(title):
+    album = {"album": {"title": title}}
+
+    try:
+        r = oauth.post('https://photoslibrary.googleapis.com/v1/albums', json=album)
+    except:
+        print "ERROR: Failed to create album!"
+        exit(-1)
+
+    return json.loads(r.text)
+
+# Function: Upload file to google photos.
+def g_uploadMedia(file, filename):
+    headers = {'Content-type': 'application/octet-stream',
+               'X-Goog-Upload-File-Name': filename,
+               'X-Goog-Upload-Protocol': 'raw'}
+
+    try:
+        f = open(file, 'rb')
+        r = oauth.post('https://photoslibrary.googleapis.com/v1/uploads', data=f.read(), headers=headers)
+        f.close()
+    except:
+        print "ERROR: Failed to upload file!"
+        exit(-1)
+
+    return r.text
+
+
+# Function: Create mediaitem from uploaded file.
+def g_createMediaItems(upload_list, albumid=None):
+    media_list = []
+    for media in upload_list:
+        item = {'description': media.values()[0],
+                'simpleMediaItem': {'uploadToken': media.keys()[0]}}
+        media_list.append(item)
+
+    if albumid is None:
+        newmediaitems = {'newMediaItems': media_list}
+    else:
+        newmediaitems = {'albumId': albumid, 'newMediaItems': media_list}
+
+    try:
+        r = oauth.post('https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate', json=newmediaitems)
+    except:
+        print "ERROR: Failed to create mediaitem."
+        exit(-1)
+
+    return json.loads(r.text)
+
+########
+# MAIN #
+########
+parser = argparse.ArgumentParser(
+    description='Use GPUcli to upload media to google photos by command line.',
+    epilog='ex. ./GPUcli.py <directory>')
+parser.add_argument('directory', help='Directory to upload.')
+parser.add_argument('-a', '--album', help='Create an album and add the uploaded. Album will be named by the <directory>.',
+    action='store_const', const=True)
+args = parser.parse_args()
+
+# Recover token data from file.
+token = load_token()
+
+# Token data not found.
+if token == "":
+    token = auth_app()
+
+# Start with a valid token.
+try:
+    oauth = OAuth2Session(client_id, token=token)
+
+    #unixtime = time.time()
+    #if(unixtime - 600 > oauth.token['expires_at']):
+    #    print "access_token expired. Refreshing..."
+    #    refresh_token()
+    userinfo = getUserInfo()
+except:
+    print "ERROR: Failed to log in!"
+    exit(-1)
+
+print "Google Photos Upload - CLI Script V" + version
+print
+print "Hi " + userinfo['given_name'] + ", we're successfully logged in! Starting your requested action!"
+print
+
+path = os.path.abspath(args.directory)
+
+# Check the source directory.
+if not os.path.isdir(path):
+    print "ERROR: Source directory does not exist!"
+    exit(-1)
+
+# Find foldername of media.
+folder = path.split(os.sep)[-1]
+if folder == "":
+    folder = "root"
+print "Uploading pictures from directory: " + path
+print "Foldername: " + folder
+print
+
+# Look at directory and identify files to upload.
+files = os.listdir(path)
+uploads = []
+for file in files:
+    filepath = path + '/' + file
+
+    if not os.path.isdir(filepath) and not file.startswith('.'):
+        print "Uploading... " + filepath
+        upload_token = g_uploadMedia(filepath, file)
+        uploads.append({upload_token: file})
+        #uploads.append({'ABC123': file})
+
+if len(uploads) > 0:
+    albumid = None
+
+    # Create album if commandline argument is given.
+    if args.album == True:
+        print "Creating album: " + folder
+        albumid = g_createAlbum(folder)
+        albumid = albumid['id']
+
+    # Finally add commit uploads to google photos.
+    upload_results = g_createMediaItems(uploads, albumid)
+
+    # Check upload results.
+    upload_ok = True
+    for result in upload_results['newMediaItemResults']:
+        if result['status']['message'] != "OK":
+            print result['mediaItem']['filename'] + ": " + result['status']['message']
+            upload_ok = False
+
+    if upload_ok == True:
+        print "Media successfully uploaded!"
+    else:
+        print "Media could not be uploaded without errors! Failed!"
+else:
+    print "ERROR: No media uploaded!"
